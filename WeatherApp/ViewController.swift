85b8489f5d164fafbe62ac5e87f62b648cc97436
Aleksey Sidorov <ealesid@alekseys-mac.local>
2019-01-19 13:16:28 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Aleksey Sidorov on 19/01/2019.
+//  Copyright © 2019 Aleksey Sidorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
