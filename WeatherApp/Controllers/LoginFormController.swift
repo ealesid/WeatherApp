9961facc76a1d9455f31dbd1b87e3e2844e3d311
Aleksey Sidorov <ealesid@alekseys-mac.local>
2019-01-26 16:14:54 +0300

    keyboard show/hide notifications

--- WeatherApp/Controllers/LoginFormController.swift
+++ WeatherApp/Controllers/LoginFormController.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+
 class LoginFormController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
@@ -10,6 +11,65 @@ class LoginFormController: UIViewController {
     private let demoUser = "user"
     private let demoPassword = "passwd"
     
+    // MARK: - Init
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        self.addNotifications()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.addNotifications()
+    }
+    
+    deinit {
+        self.removeNotifications()
+    }
+    
+    // MARK: - Notifications
+    
+    private func addNotifications() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWasShown(notification:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide(notification:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    private func removeNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWasShown(notification: Notification) {
+        guard let userInfo = notification.userInfo as NSDictionary? else {
+            return
+        }
+        guard let keyboardFrame = userInfo.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as? NSValue else {
+            return
+        }
+        
+        let keyboardHeight = keyboardFrame.cgRectValue.size.height
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardHeight, right: 0.0)
+        
+        self.scrollView?.contentInset = contentInsets
+        self.scrollView?.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc private func keyboardWillHide(notification: Notification) {
+        let contentInsets = UIEdgeInsets.zero
+        self.scrollView?.contentInset = contentInsets
+        self.scrollView?.scrollIndicatorInsets = contentInsets
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +81,8 @@ class LoginFormController: UIViewController {
         self.loginInput.underlined()
         self.passwordInput.underlined()
     }
+    
+    // MARK: - Actions
     
     @IBAction func loginButtonPressed(_ sender: UIButton) {
         guard let user = self.loginInput?.text else {
