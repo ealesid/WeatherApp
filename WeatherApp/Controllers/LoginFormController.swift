c73f3c1631a22a8ccecbe1ba15cdc277efa5ce71
Aleksey Sidorov <ealesid@alekseys-mac.local>
2019-01-26 15:21:07 +0300

    demo login

--- WeatherApp/Controllers/LoginFormController.swift
+++ WeatherApp/Controllers/LoginFormController.swift
@@ -7,6 +7,9 @@ class LoginFormController: UIViewController {
     @IBOutlet weak var loginInput: UITextField!
     @IBOutlet weak var passwordInput: UITextField!
     
+    private let demoUser = "user"
+    private let demoPassword = "passwd"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,12 +17,26 @@ class LoginFormController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        self.labelAppTitle.text = "appName"
+        self.labelAppTitle.text = "AppName"
         self.loginInput.underlined()
         self.passwordInput.underlined()
     }
     
     @IBAction func loginButtonPressed(_ sender: UIButton) {
+        guard let user = self.loginInput?.text else {
+            print("Login Input Field error")
+            return
+        }
+        guard let password = self.passwordInput?.text else {
+            print("Password Input Filed error")
+            return
+        }
+        
+        if user == self.demoUser && password == self.demoPassword {
+            print("Login successful")
+        } else {
+            print("Wrong username or password")
+        }
     }
     
 }
