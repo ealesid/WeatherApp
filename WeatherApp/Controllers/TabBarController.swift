6ea7902dc16cac60d0e32a6ca380ead11e05c61b
Aleksey Sidorov <ealesid@alekseys-mac.local>
2019-01-27 22:26:30 +0300

    1st variant completed

--- WeatherApp/Controllers/TabBarController.swift
+++ WeatherApp/Controllers/TabBarController.swift
@@ -9,22 +9,20 @@
 import UIKit
 
 class TabBarController: UITabBarController {
+    
+    private let nextViewSegues = [
+        "nextViewIsCollection",
+        "nextViewIsTable",
+    ]
 
+    @IBOutlet weak var btnNextView: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+   }
 
-        // Do any additional setup after loading the view.
+    @IBAction func btnNextViewPressed(_ sender: UIBarButtonItem) {
+        print("TabBarController", self.selectedIndex, self.selectedViewController!)
+        self.performSegue(withIdentifier: self.nextViewSegues[self.selectedIndex], sender: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
