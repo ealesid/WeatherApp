4a222902fd9fa5049daf49a8fce3e90157af99c3
Aleksey Sidorov <ealesid@mymini.local>
2019-03-17 16:14:21 +0300

    GroupsMoel images

--- WeatherApp/Models/GroupModel.swift
+++ WeatherApp/Models/GroupModel.swift
@@ -1,27 +1,6 @@
 import Foundation
 import UIKit
 
-class Group {
-    var name: String?
-    var image: UIImage?
-    
-    init(name: String) {
-        self.name = name
-    }
-    
-    init(name: String, image: UIImage = UIImage(named: "noPhoto")!) {
-        self.name = name
-        self.image = image
-    }
-}
-
-
-extension Group: Equatable {
-    static func ==(lhs: Group, rhs: Group) -> Bool {
-        return lhs.name == rhs.name
-    }
-}
-
 
 class GroupModel: Codable {
     var id: Int?
@@ -30,7 +9,12 @@ class GroupModel: Codable {
     var photo_100: String?
     var photo_200: String?
     
-    var image: UIImage?
+    var image: UIImage? {
+        guard let url = photo_50 else { return UIImage(named: "noPhoto") }
+        let imageData: Data? = try? Data(contentsOf: URL(string: url)!)
+        if let data = imageData { return UIImage(data: data) }
+        return UIImage(named: "noPhoto")
+    }
     
     enum CodingKeys: String, CodingKey {
         case id, name, photo_50, photo_100, photo_200
@@ -45,11 +29,6 @@ class GroupModel: Codable {
         photo_50 = try container.decode(String.self, forKey: .photo_50)
         photo_100 = try container.decode(String.self, forKey: .photo_100)
         photo_200 = try container.decode(String.self, forKey: .photo_200)
-    }
-    
-    init(name: String, image: UIImage = UIImage(named: "noPhoto")!) {
-        self.name = name
-        self.image = image
     }
 }
 
