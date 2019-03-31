6fa51d3a8a49a004faad69487aa9dbb2636fba96
ealesid <ealesid@gmail.com>
2019-03-31 12:22:10 +0300

    FriendsTable reloadFriends by Realm notifications

--- WeatherApp/Controllers/FriendsTableViewController.swift
+++ WeatherApp/Controllers/FriendsTableViewController.swift
@@ -5,21 +5,27 @@ import RealmSwift
 
 class FriendsTableViewController: UITableViewController {
     
-    var friends: [FriendModel] = []
-    private var selectedFriend: FriendModel?
+    var friends: Results<FriendModel>?
+    
+    var token: NotificationToken?
+    
+//    var friends: [FriendModel] = []
+//    private var selectedFriend: FriendModel?
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.reloadFriends()
+        
 //        try? FileManager.default.removeItem(at: Realm.Configuration.defaultConfiguration.fileURL!)
         
-        FriendsManager.shared.removeAllFriends()
+//        FriendsManager.shared.removeAllFriends()
         
-        FriendsManager.shared.getAllFriends { (friends: [FriendModel], error: Error?) in
-            self.friends = friends
-            OperationQueue.main.addOperation { self.tableView.reloadData() }
-        }
+//        FriendsManager.shared.getAllFriends { (friends: [FriendModel], error: Error?) in
+//            self.friends = friends
+//            OperationQueue.main.addOperation { self.tableView.reloadData() }
+//        }
 
 //        ApiManager.shared.getFriends { (response: FriendsGet?, error: Error?) in
 //            guard let friendsList = response?.response.items else { return }
@@ -30,6 +36,35 @@ class FriendsTableViewController: UITableViewController {
 //        }
         
     }
+    
+    
+    // MARK: - Reload
+    
+    func reloadFriends() {
+        var realm: Realm?
+        do {
+            realm = try Realm()
+        } catch {
+            print("\n\(#file)\n\t\(#function):\t\(#line)\n\tRealm exception")
+        }
+        
+        self.friends = realm?.objects(FriendModel.self)
+        self.token = self.friends?.observe({ (changes: RealmCollectionChange) in
+            switch changes {
+            case .initial(_):
+                self.tableView.reloadData()
+            case .update(let friends, let deletions, let insertions, let modifications):
+                self.tableView.beginUpdates()
+                self.tableView.insertRows(at: insertions.map({ IndexPath(row: $0, section: 0) }), with: .middle)
+                self.tableView.deleteRows(at: deletions.map({ IndexPath(row: $0, section: 0) }), with: .middle)
+                self.tableView.reloadRows(at: modifications.map({ IndexPath(row: $0, section: 0) }), with: .middle)
+                self.tableView.endUpdates()
+            case .error(let error):
+                print("\n\(#file)\n\t\(#function):\t\(#line)\n\tFriendsTable update error:\n\t\(error)")
+            }
+        })
+    }
+    
 
     // MARK: - Table view data source
 
@@ -38,13 +73,13 @@ class FriendsTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         return self.friends.count
+         return self.friends?.count ?? 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FriendsTableViewCell", for: indexPath) as! FriendsTableViewCell
 
-        cell.setFriend(self.friends[indexPath.row])
+        cell.setFriend(self.friends![indexPath.row])
         
         return cell
     }
@@ -71,7 +106,7 @@ class FriendsTableViewController: UITableViewController {
 
         if let identifier = segue.identifier, identifier == "showFriendInfo" {
             if let destinationVC = segue.destination  as? FriendCollectionViewController {
-                destinationVC.friend = self.friends[self.tableView.indexPathForSelectedRow!.row]
+                destinationVC.friend = self.friends![self.tableView.indexPathForSelectedRow!.row]
             }
         }
 
